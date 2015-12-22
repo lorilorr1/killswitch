9240b8916f1247c31081227a112829c5eaef0924
Richard Bagshaw <me@richardbagshaw.co.uk>
2015-12-22 17:10:07 +0000

    Remove duplicate request

--- src/KillSwitch.php
+++ src/KillSwitch.php
@@ -30,8 +30,6 @@ class KillSwitch {
                 'base_uri' => $url,
                 'timeout' => $timeout
             ]);
-
-            $this->readStatusFromHttpRequest();
         }
     }
 
